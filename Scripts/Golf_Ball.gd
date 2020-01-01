0a57c598bb803ace44ecaef1680e3e24d2e7b4ad
Greyson Jennings <tallesttower1@gmail.com>
2020-01-01 17:09:09 +0100

    oh no

--- Scripts/Golf_Ball.gd
+++ Scripts/Golf_Ball.gd
@@ -7,10 +7,17 @@ export var STATE = "still"
 
 func _ready():
 	#Load textures and things later
-	pass 
+	pass#connect("oob",self,"_on_oob")
 
 func _process(delta):
+	print(get_translation())
 	if get_linear_velocity().length() <= .01:
 		STATE = "still"
 	else:
 		STATE = "moving"
+
+func reset():
+	print("Reset " + str(get_node("Arrow").last_pos))
+	set_linear_velocity(Vector3(0,0,0))
+	set_translation(get_node("Arrow").last_pos)
+	print(get_translation())
