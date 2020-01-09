795c718185324f8b538e7f0462d1694880fb45b9
Greyson Jennings <tallesttower1@gmail.com>
2020-01-09 21:51:28 +0100

    2D particles work!!

--- Scripts/Confetti.gd
+++ Scripts/Confetti.gd
@@ -1,8 +1,9 @@
-extends Particles
+extends Particles2D
 
 func _ready():
 	get_tree().get_root().get_node("Signal_Router").connect("on_won",self,"_won")
 
 func _won():
+	set_emitting(true)
 	restart()
 	print("Confetti!!!")
