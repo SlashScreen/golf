c3fecd4fbccdd91aefc3e60d53f63d7a533b5688
Greyson Jennings <tallesttower1@gmail.com>
2020-01-15 21:38:45 +0100

    improved camera controls drastically- up/down, zoom. A bit finicky still.

--- Scripts/Golf_Camera.gd
+++ Scripts/Golf_Camera.gd
@@ -3,7 +3,9 @@ extends Camera
 export var distance = 4
 export var height = 2
 var rotator = 0 #in degrees
+var uprot = 0
 var ROT_SPEED = 3
+var SCROLL_SPEED = .5
 
 func _ready():
 	set_physics_process(true)
@@ -12,19 +14,31 @@ func _ready():
 func _physics_process(delta):
 	var target = get_parent().get_global_transform().origin
 	var pos = get_global_transform().origin
-	var up = Vector3(0,1,0)
+	var up = Vector3(0,1,0) #NTS: change up vector to gravity
 	var offset = pos - target
 	
-	if Input.is_key_pressed(KEY_A):
+	if Input.is_action_pressed("cam_left"):
 		rotator = -ROT_SPEED
-	elif Input.is_key_pressed(KEY_D):
+	elif Input.is_action_pressed("cam_right"):
 		rotator = ROT_SPEED
 	else:
 		rotator = 0
+	if Input.is_action_pressed("cam_up"):
+		uprot = ROT_SPEED
+	elif Input.is_action_pressed("cam_down"):
+		uprot = -ROT_SPEED
+	else:
+		uprot = 0
 	
-	offset = (offset.normalized().rotated(up,(rotator*(PI/180))))*distance
-	offset.y = height
+	offset = (offset.normalized().rotated(up,(rotator*(PI/180))).rotated(Vector3.RIGHT,(uprot*(PI/180))))*distance
+	#offset.y = height - fix do it doesnt drift towards ground
 	
 	pos = target + offset
 	
 	look_at_from_position(pos, target, up)
+
+func _input(event):
+	if event.is_action_pressed("cam_zoom_in"):
+		distance-=SCROLL_SPEED
+	elif event.is_action_pressed("cam_zoom_out"):
+		distance+=SCROLL_SPEED
