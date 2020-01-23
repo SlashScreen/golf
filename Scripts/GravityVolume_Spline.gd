5b5f4e7e21c5874c938d160e3d4cb7fd7e47e90a
Greyson Jennings <tallesttower1@gmail.com>
2020-01-23 20:22:53 +0100

    Success! Spline volume works correctly, and as intended. I hope. It seems to work fine here. Next, to see if the Multiplayer Glitch still persists........

--- Scripts/GravityVolume_Spline.gd
+++ Scripts/GravityVolume_Spline.gd
@@ -1,7 +1,22 @@
 extends Area
 
+export var spline_resolution = 0.0
 onready var golfBall = get_node("/root/Signal_Router").return_ball()
 
 func _process(delta):
 	if overlaps_body(golfBall):
-		set_gravity_vector(get_node("Spline").get_curve().get_closest_point(to_global(golfBall.get_ball_transform().origin)))
+		set_gravity_vector(nearest_point_on_spline(golfBall.get_ball_transform().origin,spline_resolution))
+		
+func nearest_point_on_spline(point, acc):
+	#nts: acc = accuracy, how many points to test
+	acc = float(acc)
+	var closest = INF
+	var closestPoint = Vector3()
+	for i in range(acc):
+		get_node("Spline/SplineHead").set_unit_offset(i/acc)
+		var checkVec = get_node("Spline/SplineHead").get_transform().origin
+		var d = point.distance_to(checkVec)
+		if d < closest:
+			closest = d
+			closestPoint = checkVec
+	return closestPoint
